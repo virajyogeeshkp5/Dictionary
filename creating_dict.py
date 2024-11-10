e6d45e4a8c9614703107b2afa2cb09efd14a4149
YOGEESH K P <yogeesh793@gmail.com>
2024-11-10 12:46:06 +0530

    Add files via upload

---
+++ creating_dict.py
@@ -0,0 +1,13 @@
+# Creating dictionary
+dict=([(5,9),(3,'java')])
+print(dict)
+
+ok=(((5,9),(3,'java')))
+print(ok)
+
+# using zip function
+dict=(zip((1,2,3,4,5),(10,20,30,40,50)))
+print(dict)
+
+x=dict.fromkeys([2,4,6,8,10],'even')
+print(x)
